67edeb50879cccc2cb4cbb809690d218b73151a5
184rishabh <1842001rishabsingh@gmail.com>
2021-11-01 11:32:16 +0530

    i

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,37 +1,32 @@
 import React from 'react'
-
+import './nav.css'
 function nav() {
   return (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand ps-5" href="#">Shop</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active ps-5" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Features</a>
+          <a className="nav-link active ps-5" aria-current="page" href="#">Features</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Pricing</a>
+          <a className="nav-link active ps-5" aria-current="page" href="#">Pricing</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Blog</a>
+          <a className="nav-link active ps-5" aria-current="page" href="#">Blog</a>
         </li>
         <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">Services</a>
+        <a className="nav-link active ps-5 me-5" aria-current="page" href="#">Services</a>
       </li>
-        
     </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
